2411c68f35e0d405ece740adcdf111e12b065e54
nitiantuhao <2062405236@qq.com>
2020-08-12 00:39:04 +0800

    Default Changelist

--- 测试.py
+++ 测试.py
@@ -482,4 +482,35 @@ s=student(18)
 print(s.age)
 s.age=20
 print(s.age)
-#11111
+#__init__与普通方法的验证与应用
+# s=input('1.存款 2.取款 3.转账 4.显示当前账户信息:')
+class bandcard:
+    def __init__(self):   #
+        self.moneys = 5000
+        print('当前余额',self.moneys)
+    def savemoney(self,money):
+        self.moneys=(self.moneys-money)
+        print('取款成功现余额为',self.moneys)
+    def getmoney(self,money):
+        self.moneys = (self.moneys + money)
+        print('存款成功现余额为', self.moneys)
+    def transfer(self):
+        pass
+print(' 1.存款\n 2.取款\n 3.显示当前账户信息\n 4.结束\n')
+
+while True:
+    s = input('请选择:')
+    if s=='1':
+        p=bandcard()
+        p.savemoney(int(input('输入取款金额')))
+        continue
+    elif s=='2':
+        p=bandcard()
+        p.getmoney(int(input('输入存款金额')))
+        continue
+    elif s=='3':
+        p=bandcard()
+        continue
+    elif s=='4':
+        break
+#
--- 测试3.py
+++ 测试3.py
@@ -1,33 +1,3 @@
-# s=input('1.存款 2.取款 3.转账 4.显示当前账户信息:')
-class bandcard:
-    def __init__(self):   #
-        self.moneys = 5000
-        print('当前余额',self.moneys)
-    def savemoney(self,money):
-        self.moneys=(self.moneys-money)
-        print('取款成功现余额为',self.moneys)
-    def getmoney(self,money):
-        self.moneys = (self.moneys + money)
-        print('存款成功现余额为', self.moneys)
-    def transfer(self):
-        pass
-print(' 1.存款\n 2.取款\n 3.显示当前账户信息\n 4.结束\n')
-
-while True:
-    s = input('请选择:')
-    if s=='1':
-        p=bandcard()
-        p.savemoney(int(input('输入取款金额')))
-        continue
-    elif s=='2':
-        p=bandcard()
-        p.getmoney(int(input('输入存款金额')))
-        continue
-    elif s=='3':
-        p=bandcard()
-        continue
-    elif s=='4':
-        break
 
 
 
