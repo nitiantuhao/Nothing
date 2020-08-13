070e8537dfed87c1d679c228c89e167715462d0d
nitiantuhao <2062405236@qq.com>
2020-08-13 20:23:31 +0800

    Default Changelist

--- 测试3.py
+++ 测试3.py
@@ -1,8 +1,15 @@
-class A():
-    def ss(self):
-        print('sssss')
-a=A()
-a.ss()
+class Singleton:
+    __instance=None
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance=object.__new__(cls)
+            return cls.__instance
+        else:
+            return  cls.__instance
+s=Singleton()
+print(s)
+s1=Singleton()
+print(s1)
 
 
 
