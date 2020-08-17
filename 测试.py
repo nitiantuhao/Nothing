970c7ba96092e931add883b8693dc2b63750d3f4
nitiantuhao <2062405236@qq.com>
2020-08-17 15:09:01 +0800

    Default Changelist

--- 测试.py
+++ 测试.py
@@ -560,4 +560,16 @@ s=Singleton()#第一次调用
 print(s)
 s1=Singleton()
 print(s1)
-#
+#进程的验证与应用
+from multiprocessing import Process
+def a1():
+    while True:
+        print('任务1')
+def a2():
+    while True:
+        print('任务2')
+if __name__=='__main__':
+    p=Process(target=a1)
+    p.start()
+    p1=Process(target=a2)
+    p1.start()
--- 测试3.py
+++ 测试3.py
@@ -1,6 +1,12 @@
-import re
-phone='13133670756'
-print(re.match('1\d{9}[^4,7]$',phone))
-
-
-
+from multiprocessing import Process
+def a1():
+    while True:
+        print('任务1')
+def a2():
+    while True:
+        print('任务2')
+if __name__=='__main__':
+    p=Process(target=a1)
+    p.start()
+    p1=Process(target=a2)
+    p1.start()
