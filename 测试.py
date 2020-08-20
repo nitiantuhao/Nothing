53513791d0cdfe3fb20220ad18f2848933d7907e
nitiantuhao <2062405236@qq.com>
2020-08-21 00:41:52 +0800

    Default Changelist

--- 测试.py
+++ 测试.py
@@ -482,6 +482,45 @@ s=student(18)
 print(s.age)
 s.age=20
 print(s.age)
+#保存
+class Wages_management():
+    def __init__(self):
+        self.wagess=0
+    def worker_wages(self,hour):
+        time_salaer=100
+        self.wagess=time_salaer*hour
+        return self.wagess
+    def salesman_wages(self,sales):
+        self.wagess=sales*0.2
+        return self.wagess
+    def manager_wages(self):
+        print('今月工资:12000')
+        print('请查收')
+    def sales_manager_wages(self,sales):
+        self.wagess=sales*0.3
+        return self.wagess
+print('1.工人\n2.销售员\n3.经理\n4.销售经理\n')
+while True:
+    mgs = input('清选择:')
+    if mgs=='1':
+        h=int(input('输入工作时长:'))
+        w=Wages_management()
+        print('您的工资是:',w.worker_wages(h))
+        continue
+    elif mgs=='2':
+        x=int(input('输入销售额:'))
+        w = Wages_management()
+        print('您的工资是',w.sales_manager_wages(x))
+        continue
+    elif mgs=='3':
+        w = Wages_management()
+        w.manager_wages()
+        continue
+    elif mgs=='4':
+        xs=int(input('输入销售额:'))
+        w = Wages_management()
+        print(w.sales_manager_wages(xs))
+        continue
 #__init__与普通方法的验证与应用
 # s=input('1.存款 2.取款 3.转账 4.显示当前账户信息:')
 class bandcard:
--- 测试3.py
+++ 测试3.py
@@ -1,12 +1,12 @@
 from multiprocessing import Process
 def a1():
-    while True:
+    for i in range(4):
         print('任务1')
 def a2():
-    while True:
+    for i in range(4):
         print('任务2')
 if __name__=='__main__':
-    p=Process(target=a1)
+    p=Process(target=a1,args=(1,))
     p.start()
     p1=Process(target=a2)
     p1.start()
