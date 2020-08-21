04b40126d7a135d0294e4982db3c40f452626f4f
nitiantuhao <2062405236@qq.com>
2020-08-22 01:28:08 +0800

    Default Changelist

--- 测试.py
+++ 测试.py
@@ -612,3 +612,29 @@ if __name__=='__main__':
     p.start()
     p1=Process(target=a2)
     p1.start()
+#对于第三方库gevent与协程的验证与应用
+import time
+import gevent
+from gevent import monkey
+monkey.patch_all()#起自动切换作用[猴子补丁]
+def a():#协程A
+    for i in range(4):
+        print('A'+str(i))
+        time.sleep(0.5)
+def b():#协程B
+    for i in range(4):
+        print('B'+str(i))
+        time.sleep(0.5)
+def c():#协程C
+    for i in range(4):
+        print('C'+str(i))
+        time.sleep(0.5)
+if __name__ == '__main__':
+    g1=gevent.spawn(a)#调用协程
+    g2=gevent.spawn(b)#调用协程
+    g3=gevent.spawn(c)#调用协程
+    g1.join()#堵塞出口，完成才能下一步
+    g2.join()#堵塞出口，完成才能下一步
+    g3.join()#堵塞出口，完成才能下一步
+    print('end')
+#
--- 测试3.py
+++ 测试3.py
@@ -1,12 +1,42 @@
-from multiprocessing import Process
-def a1():
-    for i in range(4):
-        print('任务1')
-def a2():
-    for i in range(4):
-        print('任务2')
-if __name__=='__main__':
-    p=Process(target=a1,args=(1,))
-    p.start()
-    p1=Process(target=a2)
-    p1.start()
+import threading
+import time
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print ("开启线程： " + self.name)
+        # 获取锁，用于线程同步
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+        # 释放锁，开启下一个线程
+        threadLock.release()
+
+def print_time(threadName, delay, counter):
+    while counter:
+        time.sleep(delay)
+        print ("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+threadLock = threading.Lock()
+threads = []
+
+# 创建新线程
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+# 开启新线程
+thread1.start()
+thread2.start()
+
+# 添加线程到线程列表
+threads.append(thread1)
+threads.append(thread2)
+
+# 等待所有线程完成
+for t in threads:
+    t.join()
+print ("退出主线程")
