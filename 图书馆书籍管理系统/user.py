fea6d52318814690e21c0d608e9e66bdecabe739
nitiantuhao <2062405236@qq.com>
2020-10-11 22:57:10 +0800

    Default Changelist

--- 图书馆书籍管理系统/user.py
+++ 图书馆书籍管理系统/user.py
@@ -4,16 +4,17 @@ def Register():#注册系统
     password = input('输入密码')
     passwordok = input('确认密码')
     if password==passwordok:
-        with open(r'C:\Users\dell\PycharmProjects\untitled\图书馆书籍管理系统\name.txt','w')as syor:
+        with open('name.txt','w')as syor:
             syor.write('{} {}\n'.format(username,password))
             print('注册成功')
+            choose()
     else:
         print('两次密码不一致')
         choose()
 def log_in():#登陆判断
     username=input('输入用户名')
     password=input('输入密码')
-    with open(r'C:\Users\dell\PycharmProjects\untitled\图书馆书籍管理系统\name.txt')as syor2:
+    with open('name.txt')as syor2:
         user_pass=('{} {}\n'.format(username,password))
         while True:
             real=syor2.readline()
